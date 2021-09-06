604df1ef014a21f3b41ec683b21ca7f025bd56d9
rodrigoinf <rodrigocampos.inf@hotmail.com>
2021-09-06 19:43:20 -0300

    Add basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App developed by <a href="https://www.linkedin.com/in/rodrigo-c" target="_blank" rel="noreferrer">Rodrigo Campos</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Bootcamp promoted by DevSuperior School: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
